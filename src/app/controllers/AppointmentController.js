dc237aa80ff91f0ba6ce20d2c06d665a1011705f
Set <6942774+setzerbomb@users.noreply.github.com>
2019-02-24 20:42:14 -0300

    Tela de visualização de compromissos como barbeiro

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,10 +1,51 @@
+const moment = require('moment')
 const { User, Appointment } = require('../models')
+const { Op } = require('sequelize')
 
 class AppointmentController {
   async create (req, res) {
     const provider = await User.findByPk(req.params.provider)
 
     return res.render('appointments/create', { provider })
+  }
+
+  async list (req, res) {
+    try {
+      let user = await User.findByPk(req.params.user)
+      if (user.provider === true) {
+        let appointments = await Appointment.findAll({
+          where: {
+            provider_id: user.id,
+            date: {
+              [Op.between]: [
+                moment(parseInt(Date.now()))
+                  .startOf('day')
+                  .format(),
+                moment(parseInt(1571047795861))
+                  .endOf('day')
+                  .format()
+              ]
+            }
+          }
+        })
+
+        let content = await Promise.all(
+          appointments.map(async ({ user_id, date }) => {
+            let { id, name, avatar } = await User.findByPk(user_id)
+            date = moment(date).format('MMMM Do YYYY, h:mm:ss a')
+            return { client: { id, name, avatar }, date }
+          })
+        )
+
+        appointments = content
+
+        return res.render('appointments/list', { appointments })
+      }
+      res.redirect('/')
+    } catch (e) {
+      console.log(e)
+      res.redirect('/')
+    }
   }
 
   async store (req, res) {
